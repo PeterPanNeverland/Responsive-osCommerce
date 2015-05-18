952fd98e54771b67cd57f80f995285ad3e3fa22e
BrockleyJohn - John Ferguson <github@yourmontage.co.uk>
2015-05-18 22:38:37 +0100

    fix duplicate #storeLogo

--- includes/modules/content/header/templates/logo.php
+++ includes/modules/content/header/templates/logo.php
@@ -1,6 +1,6 @@
 <div id="storeLogo" class="col-sm-<?php echo $content_width; ?>">
     <?php /*** Begin Header Tags SEO ***/ ?>
-     <div><div id="storeLogo">
+     <div>
       <?php echo '<a href="' . tep_href_link(FILENAME_DEFAULT) . '">' . tep_image(DIR_WS_IMAGES . 'store_logo.png', (tep_not_null($header_tags_array['logo_text']) ? $header_tags_array['logo_text'] : STORE_NAME)) . '</a>';
       if (HEADER_TAGS_DISPLAY_PAGE_TOP_TITLE == 'true') { ?>
           <div style="position:absolute; top:0; left:40%; color:#777; font-size:10px;text-align:center"><?php echo $header_tags_array['title']; ?></div>
