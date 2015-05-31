a16eef6e264f688c25086efe5ebe3cabff6e8605
BrockleyJohn - John Ferguson <github@yourmontage.co.uk>
2015-05-31 13:41:07 +0100

    Additional module for products_quantity

---
+++ includes/languages/english/modules/content/product_info/cm_pi_quantity.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2015 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+// catalog
+  define('MODULE_CONTENT_PRODUCT_INFO_QUANTITY_TEXT', 'Number in stock: ');
+
+// admin
+  define('MODULE_CONTENT_PRODUCT_INFO_QUANTITY_TITLE', 'Product Page Quantity');
+  define('MODULE_CONTENT_PRODUCT_INFO_QUANTITY_DESCRIPTION', 'Show product page quantity block on the product info page.');
---
+++ includes/modules/content/product_info/templates/quantity.php
@@ -0,0 +1,3 @@
+<div class="col-sm-<?php echo $content_width . ' ' . MODULE_CONTENT_PRODUCT_INFO_QUANTITY_CONTENT_VERT_MARGIN . ' ' . MODULE_CONTENT_PRODUCT_INFO_QUANTITY_CONTENT_HORIZ_MARGIN; ?> productsquantity">
+    <h4 class="<?php echo MODULE_CONTENT_PRODUCT_INFO_QUANTITY_CONTENT_ALIGN; ?>"><?php echo MODULE_CONTENT_PRODUCT_INFO_QUANTITY_TEXT . $products_quantity; ?></h4>
+</div>
