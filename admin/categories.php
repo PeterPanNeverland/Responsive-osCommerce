1762c224c01420a1ed646f9d2e1d4841149ca46c
BrockleyJohn - John Ferguson <github@yourmontage.co.uk>
2015-09-21 20:09:33 +0100

    add database check/fix to admin/categories

--- admin/categories.php
+++ admin/categories.php
@@ -15,6 +15,11 @@
   require(DIR_WS_CLASSES . 'currencies.php');
   $currencies = new currencies();
 
+// P2C-SORT-EDIT-1
+	if (!tep_db_num_rows(tep_db_query("SHOW COLUMNS FROM products_to_categories LIKE 'products_sort_order'"))) {
+		tep_db_query('ALTER TABLE products_to_categories ADD products_sort_order INT( 11 ) NOT NULL default 0');
+	}
+
   $action = (isset($HTTP_GET_VARS['action']) ? $HTTP_GET_VARS['action'] : '');
 
   if (tep_not_null($action)) {
@@ -862,7 +867,7 @@ $('#products_date_available').datepicker({
     if (isset($HTTP_GET_VARS['search'])) {
       $products_query = tep_db_query("select p.products_id, pd.products_name, p.products_quantity, p.products_image, p.products_price, p.products_date_added, p.products_last_modified, p.products_date_available, p.products_status, p2c.categories_id from " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd, " . TABLE_PRODUCTS_TO_CATEGORIES . " p2c where p.products_id = pd.products_id and pd.language_id = '" . (int)$languages_id . "' and p.products_id = p2c.products_id and pd.products_name like '%" . tep_db_input($search) . "%' order by pd.products_name");
     } else {
-// P2C-SORT-EDIT-1
+// P2C-SORT-EDIT-2
       $products_query = tep_db_query("select p.products_id, pd.products_name, p.products_quantity, p.products_image, p.products_price, p.products_date_added, p.products_last_modified, p.products_date_available, p.products_status from " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd, " . TABLE_PRODUCTS_TO_CATEGORIES . " p2c where p.products_id = pd.products_id and pd.language_id = '" . (int)$languages_id . "' and p.products_id = p2c.products_id and p2c.categories_id = '" . (int)$current_category_id . "' order by p2c.products_sort_order, pd.products_name");
     }
     while ($products = tep_db_fetch_array($products_query)) {
